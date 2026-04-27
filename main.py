b9d527bd2218b93a5af0b635dd7379a45d32be69
Luiz Medina <luizmedina87@gmail.com>
2026-04-27 15:01:08 -0400

    feat: add function to pretty print the sorted count of characters in the text

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from stats import count_words
+from stats import count_words, count_characters, sort_dict_on_values
 
 def get_book_text(filepath):
   with open(filepath) as f:
     file_contents = f.read()
     return file_contents
+  
+def print_list(list_of_dict):
+  for d in list_of_dict:
+    print(f"{d['name']}: {d['count']}")
 
 def main():
   book_text = get_book_text("./books/frankenstein.txt")
   word_count = count_words(book_text)
+  char_count_dict = count_characters(book_text.lower())
   print(f"Found {word_count} total words")
+  print_list(sort_dict_on_values(char_count_dict))
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,21 @@
 def count_words(text):
   return len(text.split())
+
+def count_characters(text):
+  counts= dict()
+  for char in text:
+    if char in counts:
+      counts[char] += 1
+    else:
+      counts[char] = 1
+  return counts
+
+def sort_on(items):
+  return items["count"]
+
+def sort_dict_on_values(dictionary):
+  list_of_dict = []
+  for k in dictionary:
+    list_of_dict.append({"name":k, "count":dictionary[k]})
+  list_of_dict.sort(reverse=True, key=sort_on)
+  return list_of_dict
