640408f7db17c3dd23c2ba3cb0ddc8e4391a8b61
Luiz Medina <luizmedina87@gmail.com>
2026-04-27 15:47:14 -0400

    feat: check if the sys.argv input has 2 arguments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from stats import (
   count_characters,
   sort_dict_on_values,
 )
+from sys import argv
 
 def get_book_text(filepath):
   with open(filepath) as f:
@@ -14,15 +15,18 @@ def print_list(list_of_dict):
     print(f"{d['name']}: {d['count']}")
 
 def main():
-  book_text = get_book_text("./books/frankenstein.txt")
-  word_count = count_words(book_text)
-  char_count_dict = count_characters(book_text.lower())
-  sorted_list = sort_dict_on_values(char_count_dict)
-  print("============ BOOKBOT ============")
-  print("Analyzing book found at books/frankenstein.txt...")
-  print("----------- Word Count ----------")
-  print(f"Found {word_count} total words")
-  print("--------- Character Count -------")
-  print_list(sorted_list)
+  if len(argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+  else:
+    book_text = get_book_text(argv[1])
+    word_count = count_words(book_text)
+    char_count_dict = count_characters(book_text.lower())
+    sorted_list = sort_dict_on_values(char_count_dict)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    print_list(sorted_list)
 
 main()
